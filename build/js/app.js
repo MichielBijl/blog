8830d211218f8ebf837c1ad182b113ab4d8e32f5
Michiel Bijl <m.bijl@lukkien.com>
2015-12-24 06:05:30 +0100

    Port HTML, CSS and JS

---
+++ build/js/app.js
@@ -0,0 +1,19 @@
+(function () {
+  var mainContent = document.getElementById('main');
+
+  console.log(mainContent);
+  var skipLink = document.querySelectorAll('[href="#main"]')[0];
+
+  skipLink.addEventListener('click', handleContentFocus);
+  skipLink.addEventListener('transitionend', handleContentFocus);
+
+  function handleContentFocus(event) {
+    var hasTabindex = mainContent.hasAttribute('tabindex');
+
+    if (hasTabindex) {
+      mainContent.removeAttribute('tabindex');
+    } else {
+      mainContent.setAttribute('tabindex', '-1');
+    }
+  }
+})();
